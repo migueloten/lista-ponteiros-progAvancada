3c47f49d2da93b6b5edd1620211297d79ce7be44
Severino Miguel <39276261+migueloten@users.noreply.github.com>
2021-02-20 02:20:08 -0300

    Create resposta.c

---
+++ Questão12/resposta.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(void)
+{
+  int v = 42;
+  
+  //declarando variável ponteiro
+  int *p;
+  
+  //atribuindo o endereço da variável valor ao ponteiro
+  p = &v;
+  
+  printf ("Conteudo da variavel valor: %d\n", valor);
+  printf ("Endereço da variavel valor: %x \n", &valor);
+  printf ("Conteudo da variavel ponteiro ptr: %x", ptr);
+  
+  return(0);
+}
